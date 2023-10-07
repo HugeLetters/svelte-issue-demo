a88e3527a19d71c2f290cbbad036a88ebeaf5c8a
hugeletters <ckuka71@gmail.com>
2023-10-07 20:16:40 +0200

    init

---
+++ src/routes/+page.svelte
@@ -0,0 +1,20 @@
+<script lang="ts">
+  import { onMount } from "svelte";
+  import Element, { type ElementData } from "./Element.svelte";
+
+  const values: ElementData[] = [
+    { data: 1, value: "one" },
+    { data: 2, value: "two" },
+    { data: 3, value: "three" },
+  ];
+
+  onMount(() => {
+    setTimeout(() => {
+      values[1] = { data: 4, value: "four" };
+    }, 400);
+  });
+</script>
+
+{#each values as value (value.value)}
+  <Element {value} />
+{/each}
---
+++ src/routes/Element.svelte
@@ -0,0 +1,18 @@
+<!-- <svelte:options immutable /> -->
+
+<script
+  lang="ts"
+  context="module"
+>
+  export type ElementData = { value: string; data: number };
+</script>
+
+<script lang="ts">
+  export let value: ElementData;
+
+  $: {
+    console.log("value changed", value.data, value.value);
+  }
+</script>
+
+<div>{value.data} - {value.value}</div>
